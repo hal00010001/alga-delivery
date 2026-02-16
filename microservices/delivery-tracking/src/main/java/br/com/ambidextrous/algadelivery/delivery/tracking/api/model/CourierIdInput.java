22ad9d552a5607ffe664a36bebdf70930ac79330
Almir Loiola Bindo <hal0001@gmail.com>
2026-02-16 19:05:59 -0300

    add-service

---
+++ microservices/delivery-tracking/src/main/java/br/com/ambidextrous/algadelivery/delivery/tracking/api/model/CourierIdInput.java
@@ -0,0 +1,14 @@
+package br.com.ambidextrous.algadelivery.delivery.tracking.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class CourierIdInput {
+
+    private UUID courierId;
+
+}
